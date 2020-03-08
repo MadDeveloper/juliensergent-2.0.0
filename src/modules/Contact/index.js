8eb4128c5b2851321ff7bbdb213ba352a80f188a
MadDeveloper <sergent.julien@icloud.com>
2020-03-08 23:51:15 +0100

    WIP: contact

--- src/modules/Contact/index.js
+++ src/modules/Contact/index.js
@@ -1,4 +1,7 @@
 import React from "react"
+import githubIcon from "../../assets/icons/github.svg"
+import linkedinIcon from "../../assets/icons/linkedin.svg"
+import twitterIcon from "../../assets/icons/twitter.svg"
 import { Layout } from "../../components/Layout"
 import { Typography } from "../../components/Typography"
 import styles from "./Contact.module.css"
@@ -8,9 +11,10 @@ export function Contact() {
   return (
     <Layout.Main>
       <Layout.Content>
-        <Typography.Paragraph heading>
+        <Typography.Title heading>Hi, I'm Julien.</Typography.Title>
+        <Typography.Paragraph>
           <Typography.Text>
-            Hi, I'm Julien, a software engineer based in France.
+            A software engineer based in France.
             <br />I started to code when I was 11. I always had a taste for
             well-written code, design systems, attention to details, making each
             view an incredible experience, both in terms of UX and UI.
@@ -22,13 +26,59 @@ export function Contact() {
             frontend or backend developer, I can only be a fullstack developer.
           </Typography.Text>
         </Typography.Paragraph>
-        <Typography.Title>How to contact me</Typography.Title>
+        <Typography.Subtitle>How to contact me</Typography.Subtitle>
         <Typography.Paragraph>
-          <Typography.Text>You can use the form below</Typography.Text>
+          <Typography.Text>You can use the form below:</Typography.Text>
         </Typography.Paragraph>
         <div className={styles.form}>
           <ContactForm />
         </div>
+        <Typography.Paragraph>
+          <Typography.Text>
+            Or you can contact me directly at{" "}
+            <a href="mailto:sergent.julien@icloud.com">
+              sergent.julien@icloud.com
+            </a>
+            .
+          </Typography.Text>
+        </Typography.Paragraph>
+        <Typography.Paragraph>
+          <Typography.Text>
+            You can also find me around the web:
+          </Typography.Text>
+        </Typography.Paragraph>
+        <ul className={styles.social}>
+          <li className={styles.item}>
+            <a
+              href="https://github.com/MadDeveloper"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src={githubIcon} className={styles.icon} alt="" />
+              <span>MadDeveloper</span>
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a
+              href="https://www.linkedin.com/in/julien-sergent/"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src={linkedinIcon} className={styles.icon} alt="" />
+              <span>julien-sergent</span>
+            </a>
+          </li>
+          <li className={styles.item}>
+            <a
+              href="https://twitter.com/_maddeveloper"
+              target="_blank"
+              rel="noopener"
+            >
+              <img src={twitterIcon} className={styles.icon} alt="" />
+              <span>_maddeveloper</span>
+            </a>
+          </li>
+        </ul>
       </Layout.Content>
     </Layout.Main>
   )
