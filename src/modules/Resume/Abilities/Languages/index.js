77aeca1ad5566489550020be981c778897277d77
MadDeveloper <sergent.julien@icloud.com>
2020-03-19 22:46:13 +0100

    feat(resume): add languages

--- src/modules/Resume/Abilities/Languages/index.js
+++ src/modules/Resume/Abilities/Languages/index.js
@@ -1,17 +1,39 @@
 import React from "react"
+import enFlag from "../../../../assets/images/flags/en.jpg"
+import frFlag from "../../../../assets/images/flags/fr.jpg"
 import { Typography } from "../../../../components/Typography"
+import styles from "./Languages.module.css"
 
 export function Languages() {
   return (
     <article>
       <Typography.Subtitle>Languages</Typography.Subtitle>
-      <Typography.Paragraph>
-        <Typography.Text>Todo</Typography.Text>
-      </Typography.Paragraph>
-      <ul>
-        <li>French (native)</li>
-        <li>English (daily use)</li>
-      </ul>
+      <div className={styles.languages}>
+        <div className={styles.lang}>
+          <img src={frFlag} alt="fr flag" className={styles.flag} />
+          <div>
+            <div>
+              <Typography.Text bold>French</Typography.Text>
+            </div>
+            <div>
+              <Typography.Text secondary>Native language.</Typography.Text>
+            </div>
+          </div>
+        </div>
+        <div className={styles.lang}>
+          <img src={enFlag} alt="en flag" className={styles.flag} />
+          <div>
+            <div>
+              <Typography.Text bold>English</Typography.Text>
+            </div>
+            <div>
+              <Typography.Text secondary>
+                Daily use, professional experiences.
+              </Typography.Text>
+            </div>
+          </div>
+        </div>
+      </div>
     </article>
   )
 }
