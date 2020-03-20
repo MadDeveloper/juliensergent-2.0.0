e00f2d1bf09520c192164ae1df27fd853fde025f
MadDeveloper <sergent.julien@icloud.com>
2020-03-20 17:02:58 +0100

    feat(Resume): add photo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,19 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import { config } from "../../config"
 import { Typography } from "../Typography"
 import styles from "./Header.module.css"
 import { Menu } from "./Menu"
 
 export function Header() {
+  const location = useLocation()
+
   return (
     <header className={styles.root}>
       <Link to="/" className={styles.nameLink}>
-        <div className={styles.photo} />
+        {location.pathname !== config.routes.resume && (
+          <div className={styles.photo} />
+        )}
         <Typography.Title className={styles.name}>
           Julien Sergent
         </Typography.Title>
--- src/modules/Resume/Introduction.js
+++ src/modules/Resume/Introduction.js
@@ -2,66 +2,72 @@ import React from "react"
 import { Layout } from "../../components/Layout"
 import { Typography } from "../../components/Typography"
 import { calculateAge } from "../../lib/time"
+import styles from "./Introduction.module.css"
 import { TechnologyList } from "./TechnologyList"
 
 export function Introduction() {
   return (
     <Layout.Content>
-      <Typography.Paragraph>
-        <Typography.Text>
-          Hi, I'm <Typography.Text bold>Julien</Typography.Text>, I'm{" "}
-          <Typography.Text bold>
-            {calculateAge(new Date(1995, 9, 18))}
-          </Typography.Text>
-          , and I'm a <Typography.Text bold>software engineer</Typography.Text>{" "}
-          based in France.
-        </Typography.Text>
-      </Typography.Paragraph>
-      <Typography.Paragraph>
-        <Typography.Text>
-          I started to code when I was 11. I always had a taste for well-written
-          code, design systems, attention to details, making each view an
-          incredible experience, both in terms of UX and UI.
-        </Typography.Text>
-      </Typography.Paragraph>
-      <Typography.Paragraph>
-        <Typography.Text>
-          My natural addiction towards the design, prevents me to be just a
-          frontend or backend developer, I can only be a fullstack developer.
-        </Typography.Text>
-      </Typography.Paragraph>
-      <div>
-        <Typography.Subtitle>Skills quick look</Typography.Subtitle>
-        <TechnologyList
-          compact
-          list={[
-            {
-              name: "JavaScript",
-              path: "javascript"
-            },
-            {
-              name: "Node.js",
-              path: "node"
-            },
-            {
-              name: "React",
-              path: "react"
-            },
-            {
-              name: "React Native",
-              path: "react"
-            },
-            {
-              name: "MongoDB",
-              path: "mongodb"
-            },
-            {
-              name: "GraphQL",
-              path: "graphql"
-            }
-          ]}
-        />
+      <div className={styles.root}>
+        <div>
+          <Typography.Paragraph>
+            <Typography.Text>
+              Hi, I'm <Typography.Text bold>Julien</Typography.Text>, I'm{" "}
+              <Typography.Text bold>
+                {calculateAge(new Date(1995, 9, 18))}
+              </Typography.Text>
+              , and I'm a{" "}
+              <Typography.Text bold>software engineer</Typography.Text> based in
+              France.
+            </Typography.Text>
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            <Typography.Text>
+              I started to code when I was 11. I always had a taste for
+              well-written code, design systems, attention to details, making
+              each view an incredible experience, both in terms of UX and UI.
+            </Typography.Text>
+          </Typography.Paragraph>
+          <Typography.Paragraph>
+            <Typography.Text>
+              My natural addiction towards the design, prevents me to be just a
+              frontend or backend developer, I can only be a fullstack
+              developer.
+            </Typography.Text>
+          </Typography.Paragraph>
+        </div>
+        <div className={styles.photo}></div>
       </div>
+      <Typography.Subtitle>Skills quick look</Typography.Subtitle>
+      <TechnologyList
+        compact
+        list={[
+          {
+            name: "JavaScript",
+            path: "javascript"
+          },
+          {
+            name: "Node.js",
+            path: "node"
+          },
+          {
+            name: "React",
+            path: "react"
+          },
+          {
+            name: "React Native",
+            path: "react"
+          },
+          {
+            name: "MongoDB",
+            path: "mongodb"
+          },
+          {
+            name: "GraphQL",
+            path: "graphql"
+          }
+        ]}
+      />
     </Layout.Content>
   )
 }
